d9b0a7274ac7dce8df06b7d65e934e006f9bbf15
Eddy <eddy.pathit@gmail.com>
2023-07-10 19:40:16 +0700

    feat: support multiple oa

--- src/controllers/lineHookC.ts
+++ src/controllers/lineHookC.ts
@@ -5,22 +5,49 @@ import { LineWebhook } from '../models/linehookT'
 
 
 const lineHook = async (req: Request, res: Response, next: NextFunction) => {
+    const data: LineWebhook = req.body
+    let oaBid: Buffer
+    let oaSecret: string
+    let oaToken: string
+    // validate that OA is in our care
+    try {
+        const destination = data.destination
+        const oaResult = await lineHookS.checkOAClient(destination)
+
+        if (oaResult[0] === true) {
+            oaBid = oaResult[1]["line_oa_id_bin"]
+            oaSecret = oaResult[1]["line_message_secret"]
+            oaToken = oaResult[1]["line_message_token"]
+        } else {
+            res.status(400).send('H@@krec3ive')
+            next()
+            return
+        }
+
+    } catch (error) {
+        next(error)
+        return
+    }
+
+
     if (await validator.verifyLineWebhookSignature(req) == true) {
-        const data: LineWebhook = req.body
+
+
+
         const lineUser: string = data.events[0].source.userId
         const bid: Buffer = await lineHookS.userHandler(lineUser)
 
         const event: string = data.events[0].type
         switch (event) {
             case "message":
-                lineHookS.logActivity(bid, "message")
+                lineHookS.logActivity(bid, oaBid, "message")
                 break;
             case "follow":
-                lineHookS.logActivity(bid, "follow")
+                lineHookS.logActivity(bid, oaBid, "follow")
                 // do some follow such as send flex message
                 break
             case "unfollow":
-                lineHookS.logActivity(bid, "unfollow")
+                lineHookS.logActivity(bid, oaBid, "unfollow")
                 // do some unfollow
                 break
             case "postback":
