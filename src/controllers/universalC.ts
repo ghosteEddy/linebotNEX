79a4d3aa9556f2a5bace8b3dfb4213f8d01a25de
Eddy <eddy.pathit@gmail.com>
2023-07-19 18:09:49 +0700

    stable

--- src/controllers/universalC.ts
+++ src/controllers/universalC.ts
@@ -3,6 +3,11 @@ import ulogS from '../services/universalLogS'
 
 const healthCheck = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        if (process.env.NODE_ENV == "development") {
+            console.log(req.socket.remoteAddress)
+            console.log(req.headers)
+            console.log(req.body)
+        }
         res.status(200).send("I'm healthy")
     } catch (error) {
         next(error)
@@ -12,6 +17,10 @@ const healthCheck = async (req: Request, res: Response, next: NextFunction) => {
 
 const ulog = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        let client = req.socket.remoteAddress
+        // check env reverse proxy()
+        if (req.headers["x-forwarded-for"]) { client = String(req.headers["x-forwarded-for"]) }
+        if (req.body.remark == null) { req.body.remark = `remote address: ${client}\n headers: ${JSON.stringify(req.headers)}` }
         ulogS.log(req.body)
         res.status(200).send()
     } catch (error) {
